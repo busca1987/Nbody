bdc0323a93a6f58aa100b587f441d38ced845196
Piero <Piero@192.168.0.8>
2014-05-05 16:44:03 +0200

    abc

--- NBody/src/pap1213/assignment/nbody/Body.java
+++ NBody/src/pap1213/assignment/nbody/Body.java
@@ -10,7 +10,7 @@ public class Body extends Thread {
 	private static final double dt = 20;
 	//private static final double solarmass=1.98892e30;
 	
-	private P2d pos; 		  // cartesian position
+	private P2d pos; 		  // cartesian positionsss
 	private V2d vel;       	 // velocity 
 	public double fx, fy; 	// force
 	public double mass;    // mass
